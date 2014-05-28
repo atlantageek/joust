e5db30cd4eb5d6226b3aa2073e568e542b7fe3a2
atlantageek <atlantageek@gmail.com>
2014-05-27 22:38:59 -0400

    update

--- game/states/gameover.js
+++ game/states/gameover.js
@@ -20,7 +20,7 @@ GameOver.prototype = {
   },
   update: function () {
     if(this.game.input.activePointer.justPressed()) {
-      this.game.state.start('play');
+      this.game.state.start('play', true, true);
     }
 
   }
